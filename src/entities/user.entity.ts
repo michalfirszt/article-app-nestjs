e5898df6ae54ebb1ef3634e6cc2274fc422af59a
michalfirszt <michalfiszu@gmail.com>
2021-06-03 20:49:37 +0200

    User entity

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,44 @@
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Index('users-email-unique', ['email'], { unique: true })
+  @Column()
+  email: string;
+
+  @Column({
+    name: 'email_verified_at',
+    type: 'timestamp',
+    default: null,
+    nullable: true,
+  })
+  emailVerifiedAt: Date | null;
+
+  @Column({ nullable: true })
+  password: string | null;
+
+  @Column({ default: true })
+  active: boolean;
+
+  @Column({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    nullable: true,
+  })
+  createdAt: Date | null;
+
+  @Column({
+    name: 'updated_at',
+    type: 'timestamp',
+    default: null,
+    nullable: true,
+  })
+  updatedAt: Date | null;
+}
