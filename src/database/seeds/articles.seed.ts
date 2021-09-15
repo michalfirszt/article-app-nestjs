dd4b57c2c1344ce892e92ad836fa6de5ffcd8cec
michalfirszt <michalfiszu@gmail.com>
2021-09-15 11:42:59 +0200

    Assign user to article seeder

--- src/database/seeds/articles.seed.ts
+++ src/database/seeds/articles.seed.ts
@@ -1,9 +1,24 @@
 import { Factory, Seeder } from 'typeorm-seeding';
 import { Connection } from 'typeorm';
-import { Article } from '../../entities';
+import { Article, User } from '../../entities';
 
 export default class CreateArticles implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<any> {
-    await factory(Article)().createMany(10);
+    const userIds = await connection
+      .getRepository(User)
+      .createQueryBuilder('user')
+      .select('user.id')
+      .getMany();
+    const ids: number[] = userIds.map(({ id }) => id);
+
+    await factory(Article)()
+      .map(async (article: Article): Promise<Article> => {
+        const user = new User();
+        user.id = ids[Math.floor(Math.random() * ids.length)];
+        article.user = user;
+
+        return article;
+      })
+      .createMany(5);
   }
 }
